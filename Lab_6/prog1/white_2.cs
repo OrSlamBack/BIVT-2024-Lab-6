46821841e6998db29c9c49573cee0404f37ddfb2
OrSlamBack <4ebyrekman@gmail.com>
2025-04-25 20:08:32 +0300

    Update white_2.cs

--- Lab_6/prog1/white_2.cs
+++ Lab_6/prog1/white_2.cs
@@ -6,54 +6,55 @@ using System.Text;
 using System.Threading.Tasks;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
+
 namespace Lab_6
 {
-    public class White_1
+    public class White_2
     {
         public struct Participant
         {
-            private string surname, club;
+            private string surname, name;
             private double firstJump, secondJump;
-            public string Surname  { get { return surname; } }
-            public string Club { get { return club; } }
+            public string Name { get { return name; } }
+            public string Surname { get { return surname; } }
             public double FirstJump { get { return firstJump; } }
             public double SecondJump { get { return secondJump; } }
 
-            public double JumpSum { get { return (FirstJump + SecondJump); } }
-            public Participant(string _Surname, string _Club)
+            public double BestJump { get { return Math.Max(FirstJump, SecondJump); } }
+            public Participant(string _Name,string _Surname)
             {
                 surname = _Surname;
-                club = _Club;
-                firstJump = 0;
-                secondJump = 0;
+                name = _Name;
+                firstJump = 0; secondJump = 0;
+            }
+            public void Participant_(string _Name, string _Surname)
+            {
+                surname = _Surname;
+                name = _Name;
+                firstJump = 0; secondJump = 0;
             }
             public void Jump(double result)
             {
-                    if (firstJump == 0)
-                    {
-                        firstJump = result;
-                    }
-
-                else if (secondJump == 0)
+                if (FirstJump == 0)
+                {
+                    firstJump = result;
+                }
+                else if (SecondJump == 0)
                 {
                     secondJump = result;
                 }
             }
             public void Print()
             {
-                if(Surname == null || Club == null)
-                {
-                    return;
-                }
-                Console.Write(Surname);
+                if (Surname == null || Name == null) return;
+                Console.Write(Name);
                 Console.Write(" ");
-                Console.Write(Club);
+                Console.Write(Surname);
                 Console.Write(" ");
                 Console.Write(FirstJump);
                 Console.Write(" ");
                 Console.WriteLine(SecondJump);
             }
-
             public static void Sort(Participant[] array)
             {
                 if (array == null)
@@ -64,7 +65,7 @@ namespace Lab_6
                 {
                     for (int j = i + 1; j < array.Length; j++)
                     {
-                        if (array[j].JumpSum > array[i].JumpSum)
+                        if (array[j].BestJump > array[i].BestJump)
                         {
                             (array[i], array[j]) = (array[j], array[i]);
                         }
